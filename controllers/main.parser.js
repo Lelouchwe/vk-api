3e77e26833573349a279ccaeac78357426d641d3
Vitaliy Shubnikov <vitalikshubnikov@icloud.com>
2020-06-05 03:14:40 +0300

    comments

--- controllers/main.parser.js
+++ controllers/main.parser.js
@@ -10,6 +10,8 @@ let getDialogs = async (access, start_message_id) => {
     }
    let {response} = await request(options)
    let list = []
+//    Поскольку метод возвращает массив объектов диалогов, с различными метаданными, а нам понадобяться только их id
+//    извлечем их добавим в массив
     for(let item of response.items){
         list.push(item.message.user_id)
     }
@@ -18,7 +20,7 @@ let getDialogs = async (access, start_message_id) => {
 let getHistory = async (access, userID) => {
     const options = {
         method: 'GET',
-        uri: `https://api.vk.com/method/messages.getHistory?v=5.41&access_token=${access}&user_id=${userID}&offset=0`,
+        uri: `https://api.vk.com/method/messages.getHistory?v=5.41&access_token=${access}&count=200&user_id=${userID}&offset=0`,
         json: true,
     }
     let {response} = await request(options)
@@ -45,9 +47,9 @@ let parse = async () => {
     // await fs.writeFile('alldialogs.txt', allDialogs, (error, data) => console.log(data))
     let reading = await fs.readFileSync('alldialogs.txt', 'utf-8').split(',')
 
-    for(let i=8; i<11; i++){
+    for(let i=170; i<180; i++){
         let filterMassage = [], tempArr = [], numbArr = []
-        let dialog = await getHistory(access_token,reading[i])
+        let dialog = await getHistory(access_token,reading[i],200)
         let question = ''
         let answere = ''
         let predOut = 0
@@ -75,32 +77,19 @@ let parse = async () => {
                 question = ''
             }
         }
+        // поскольку вместе с сообщениями сохраняются символы переноса строк, которые будут мешать в дальнейшем, необходимо из сразу убрать. С помощью цикла получаем каждое сообщение и регулярным выражением убираем ненужные символы
         numbArr.forEach(item => filterMassage.push(item.replace(/\n/g, ' ').slice(0,-2)))
+        // в диалогах могут встречаться не нужные слова по типа "спасибо", которые не еффективны для обучающей выборки. Поскольку такие сообщения всегда являются последними, на которые не последует ответа, или же диалоги в которых нет ответа, являются нечетными, поэтому убираем их
         if(filterMassage.length % 2 !== 0 )
             filterMassage.pop()
+        // заменяем все встречающиеся в тексте двойные ковычки на одинарные т.к. они помешают в формирование csv файла
         filterMassage = filterMassage.map(item => item.replace(/"/g, "'") )
         console.log(filterMassage)
+        // сохраням полученые диалоги в csv файле на локальной машине
         for(let i = 0; i<filterMassage.length; i+=2){
-            // allDialogs.push(`"${filterMassage[i]}" , "${filterMassage[i+1]}"\n`) 
             await fs.appendFileSync('datasetvyatsu.csv', `"${filterMassage[i]}","${filterMassage[i+1]}"\n`)
         }
-        // allDialogs.push(`"${question}","${answere}"\n`)
-        // allDialogs.push(`${question}`+','+`${answere}`)
-        // console.log(tempArr)
-        // console.log(filterMassage)
-
-        // console.log(dialog);
-        
     }
-    // await fs.writeFileSync('datasetvyatsu.txt', allDialogs)
-    // let allMassages = []
-    // for (let item of list) {
-    //     allMassages.push(await getHistory(item))
-    // }
-    // console.log(allMassages)
-    // let list = await getCountMessage(access_token)
-    // console.log(list)
-    // console.log(allDialogs)
 }
 
 
